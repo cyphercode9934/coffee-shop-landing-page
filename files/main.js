9b48eae1383640819cd163e37e2fa4e225c44b05
cyphercode9934 <cyphercode9934@gmail.com>
2024-10-27 05:36:48 -0700

    Add files via upload

---
+++ files/main.js
@@ -0,0 +1,44 @@
+//Go to top button
+window.addEventListener('scroll', scrollFunction)
+
+function scrollFunction(){
+    const goTopBtn = document.querySelector('.go_top')
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        goTopBtn.style.display = "block"
+    }
+    else{
+        goTopBtn.style.display = "none"
+    }
+}
+
+//Sidebar
+function topFunction() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar_container')
+    sidebar.classList.add('sidebar_active')
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar_container')
+    sidebar.classList.remove('sidebar_active')
+}
+
+//Navbar scrolling
+let lastScrollTop = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY || document.documentElement.scrollTop;
+    const navbar = document.querySelector(".nav_container")
+    if(currentScroll > lastScrollTop){
+        navbar.classList.add('nav_hidden')
+    }
+    else{
+        navbar.classList.remove('nav_hidden')
+    }
+    
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
